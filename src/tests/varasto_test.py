7b82688a4e15150fe21839fe1b0277a803bdc369
Erik Mende <erikpeik@outlook.com>
2025-10-31 01:35:03 +0200

    Unittest and Poetry exercise

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,48 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_invalid_lisays_ei_muuta_saldoa(self):
+        self.varasto.lisaa_varastoon(-5)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisays_yli_tilavuuden_tayttaa_varaston(self):
+        self.varasto.lisaa_varastoon(15)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_invalid_otto_ei_muuta_saldoa(self):
+        self.varasto.lisaa_varastoon(8)
+        self.varasto.ota_varastosta(-3)
+
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_otto_yli_saldon_tyhjentaa_varaston(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu_maara = self.varasto.ota_varastosta(10)
+
+        self.assertAlmostEqual(saatu_maara, 8)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_konstruktori_negatiivinen_tilavuus_nollaa_tilavuuden(self):
+        negatiivinen_varasto = Varasto(-5)
+
+        self.assertAlmostEqual(negatiivinen_varasto.tilavuus, 0)
+
+    def test_konstruktori_negatiivinen_alku_saldo_nollaa_saldon(self):
+        negatiivinen_varasto = Varasto(10, -3)
+
+        self.assertAlmostEqual(negatiivinen_varasto.saldo, 0)
+
+    def test_str_metodi_toimii_oikein(self):
+        # Tyhjä varasto
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+
+        # Varasto johon lisätty tavaraa
+        self.varasto.lisaa_varastoon(3)
+        self.assertEqual(str(self.varasto), "saldo = 3, vielä tilaa 7")
+
+        # Täysi varasto
+        self.varasto.lisaa_varastoon(7)
+        self.assertEqual(str(self.varasto), "saldo = 10, vielä tilaa 0")
