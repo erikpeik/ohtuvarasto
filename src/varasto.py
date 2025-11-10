3cc2ce5a98e53f936e51b64659e107122298a286
Erik Mende <erikpeik@outlook.com>
2025-11-10 22:28:10 +0200

    rollbacking to fix pylint

--- src/varasto.py
+++ src/varasto.py
@@ -19,7 +19,8 @@ class Varasto:
         # täyteen ja ylimäärä hukkaan!
         return tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
