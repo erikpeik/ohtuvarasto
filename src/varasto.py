c0d151380fab93a79ed2d6265e5f3bfeeb250190
Erik Mende <erikpeik@outlook.com>
2025-11-10 22:27:24 +0200

    adding too long line to broke pylint

--- src/varasto.py
+++ src/varasto.py
@@ -19,8 +19,7 @@ class Varasto:
         # täyteen ja ylimäärä hukkaan!
         return tilavuus
 
-    # huom: ominaisuus voidaan myös laskea.
-    # Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
